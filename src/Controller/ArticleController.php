574eaa67546b9d5531be0a58ace104b64bdacb41
Alexandre Canivez <alexandre.canivez@gmail.com>
2018-10-01 15:47:51 +0200

    add some templates

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -15,17 +15,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      *
      * @return Response
      */
     public function homepage()
     {
-        return new Response('OMG MY FIRST PAGE');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
