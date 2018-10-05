d707a54153319372c934298a5b25fd2275027d90
Alexandre Canivez <alexandre.canivez@gmail.com>
2018-10-05 17:20:45 +0200

    Giving the Comments an isDeleted Flag

--- src/DataFixtures/CommentFixture.php
+++ src/DataFixtures/CommentFixture.php
@@ -16,10 +16,11 @@ class CommentFixture extends BaseFixture implements DependentFixtureInterface
     {
         $this->createMany(Comment::class, 100, function (Comment $comment) {
             $comment->setContent(
-                $this->faker->boolean ? $this->faker->paragraph : $this->faker->sentences(4, true)
+                $this->faker->boolean ? $this->faker->paragraph : $this->faker->sentences(2, true)
             );
             $comment->setAuthorName($this->faker->name);
             $comment->setCreatedAt($this->faker->dateTimeBetween('-1 months', '-1 seconds'));
+            $comment->setIsDeleted($this->faker->boolean(20));
 //            $comment->setArticle($this->getReference(Article::class.'_'.$this->faker->numberBetween(0, 9)));
             $comment->setArticle($this->getRandomReference(Article::class));
         });
