451a344ba141e5eba790d008a81302c278859784
Alexandre Canivez <alexandre.canivez@gmail.com>
2019-01-17 11:06:29 +0100

    create ApiToken Entity

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\ApiToken;
 use App\Entity\User;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -23,13 +24,19 @@ class UserFixture extends BaseFixture
      */
     protected function loadData(ObjectManager $manager): void
     {
-        $this->createMany('10', 'main_users', function ($i) {
+        $this->createMany('10', 'main_users', function ($i) use ($manager) {
             $user = new User();
             $user->setEmail(sprintf('spacebar%d@example.com', $i));
             $user->setFirstName($this->faker->firstName);
             if ($this->faker->boolean) {
                 $user->setTwitterUsername($this->faker->userName);
             }
+            // Add use $manager to the method for persist ApiToken instantiated
+            $apiToken1 = new ApiToken($user);
+            $apiToken2 = new ApiToken($user);
+            $manager->persist($apiToken1);
+            $manager->persist($apiToken2);
+
             $user->setPassword($this->passwordEncoder->encodePassword(
                 $user, // Pour savoir quel algorythme on a mis dans sécurity
                 'engage'
