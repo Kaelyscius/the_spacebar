03bbde740e927eea75629eed763fdbf346868c3a
Alexandre Canivez <alexandre.canivez@gmail.com>
2019-01-21 10:26:41 +0100

    Agree to Terms Database Field

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -67,6 +67,11 @@ class User implements UserInterface
     private $articles;
 
     /**
+     * @ORM\Column(type="datetime")
+     */
+    private $agreedTermsAt;
+
+    /**
      * User constructor.
      */
     public function __construct()
@@ -318,5 +323,17 @@ class User implements UserInterface
     public function __toString()
     {
         return $this->getFirstName();
+    }
+
+    public function getAgreedTermsAt(): ?\DateTimeInterface
+    {
+        return $this->agreedTermsAt;
+    }
+
+    public function setAgreedTermsAt(\DateTimeInterface $agreedTermsAt): self
+    {
+        $this->agreedTermsAt = $agreedTermsAt;
+
+        return $this;
     }
 }
