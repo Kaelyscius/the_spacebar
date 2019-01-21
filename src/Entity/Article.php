b2d9c16ffc5975835ef63539d92b39a422ae058a
Alexandre Canivez <alexandre.canivez@gmail.com>
2019-01-21 17:01:20 +0100

    Setup: For Dependent Select Fields

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -79,6 +79,16 @@ class Article
     private $author;
 
     /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $location;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $specificLocationName;
+
+    /**
      * Article constructor.
      */
     public function __construct()
@@ -361,5 +371,29 @@ class Article
                 ->atPath('title')
                 ->addViolation();
         }
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(?string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getSpecificLocationName(): ?string
+    {
+        return $this->specificLocationName;
+    }
+
+    public function setSpecificLocationName(?string $specificLocationName): self
+    {
+        $this->specificLocationName = $specificLocationName;
+
+        return $this;
     }
 }
