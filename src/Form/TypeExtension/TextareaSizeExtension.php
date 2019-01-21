56da169547ede887ac533e83fd0bacc8dc316cfa
Alexandre Canivez <alexandre.canivez@gmail.com>
2019-01-21 15:19:03 +0100

    Form Type Extension

---
+++ src/Form/TypeExtension/TextareaSizeExtension.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Form\TypeExtension;
+
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormTypeExtensionInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class TextareaSizeExtension.
+ */
+class TextareaSizeExtension implements FormTypeExtensionInterface
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+    }
+
+    /**
+     * @param FormView      $view
+     * @param FormInterface $form
+     * @param array         $options
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['attr']['rows'] = $options['rows'];
+    }
+
+    /**
+     * @param FormView      $view
+     * @param FormInterface $form
+     * @param array         $options
+     */
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'rows' => 10,
+        ]);
+    }
+
+    /**
+     * @return string
+     */
+    public function getExtendedType()
+    {
+        return TextareaType::class;
+    }
+}
