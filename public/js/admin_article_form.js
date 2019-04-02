314fefec50e9f20b7f301940f95e1c5fd04632b1
Alexandre Canivez <alexandre.canivez@gmail.com>
2019-04-02 10:14:23 +0200

    Reordering the Files

--- public/js/admin_article_form.js
+++ public/js/admin_article_form.js
@@ -36,6 +36,10 @@ class ReferenceList
 {
     constructor($element) {
         this.$element = $element;
+        this.sortable = Sortable.create(this.$element[0], {
+            handle: '.drag-handle',
+            animation: 150,
+        });
         this.references = [];
         this.render();
 
@@ -95,6 +99,7 @@ class ReferenceList
         const itemsHtml = this.references.map(reference => {
             return `
 <li class="list-group-item d-flex justify-content-between align-items-center" data-id="${reference.id}">
+    <span class="drag-handle fa fa-reorder"></span>
     <input type="text" value="${reference.originalFilename}" class="form-control js-edit-filename" style="width: auto;">
 
     <span>
